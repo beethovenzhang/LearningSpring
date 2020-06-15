32c728a0d6798bc82cdb952158daf84386ede61a
Yunfei Zhang <beethovenzhang@gmail.com>
2020-06-14 17:52:49 -0700

    Use autowire annotation for dependency injection

---
+++ Spring-Demo-Annotation/src/com/allen/springdemo/FortuneService.java
@@ -0,0 +1,5 @@
+package com.allen.springdemo;
+
+public interface FortuneService {
+    public String getFortune();
+}
--- Spring-Demo-Annotation/src/com/allen/springdemo/TennisCoach.java
+++ Spring-Demo-Annotation/src/com/allen/springdemo/TennisCoach.java
@@ -1,11 +1,26 @@
 package com.allen.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    // automatically find component with matching types
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
