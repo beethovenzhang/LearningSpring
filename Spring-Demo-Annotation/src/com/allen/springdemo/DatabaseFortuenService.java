59df43c627c56f5bcebe7f3bf3d7a1fe0ad4128d
Yunfei Zhang <beethovenzhang@gmail.com>
2020-06-17 15:49:51 -0700

    Use qualifier for dependency injection

---
+++ Spring-Demo-Annotation/src/com/allen/springdemo/DatabaseFortuenService.java
@@ -0,0 +1,11 @@
+package com.allen.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuenService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
---
+++ Spring-Demo-Annotation/src/com/allen/springdemo/RestFortuenService.java
@@ -0,0 +1,11 @@
+package com.allen.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestFortuenService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
